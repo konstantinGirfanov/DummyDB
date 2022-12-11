93e96b2cbd0eef70110a7c851e08b9593f8aa107
Kostya <super_serega_2007@mail.ru>
2022-12-12 00:47:18 +0500

    DB1

---
+++ Reader.cs
@@ -0,0 +1,14 @@
+﻿namespace DummyDB
+{
+    class Reader
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Reader(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
